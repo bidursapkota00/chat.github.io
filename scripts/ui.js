7fe13495da01923803b600be210067bd3a0820c7
bidursapkota00 <62147082+bidursapkota00@users.noreply.github.com>
2021-02-20 19:11:32 +0545

    date fns is insecure

--- scripts/ui.js
+++ scripts/ui.js
@@ -12,7 +12,16 @@ class ChatUI{
         //const when = dateFns.distanceInWordsToNow(
            // data.created_at.toDate(),
             //{ addSuffix: true }
-        const when = data.created_at.toDate();
+        //const when = data.created_at.toDate();
+        
+        let when;
+        if(data.created_at.toDate().getHours() < 12){
+            when = data.created_at.toDate().getHours() + " : " + data.created_at.toDate().getMinutes() + " am";
+        }else if(data.created_at.toDate().getHours() > 12){
+            when = (data.created_at.toDate().getHours()-12) + " : " + data.created_at.toDate().getMinutes() + " pm";
+        }else{
+            when = data.created_at.toDate().getHours() + " : " + data.created_at.toDate().getMinutes() + " pm";
+        }
         
         let bool = data.username == localStorage.getItem('username') ? 1 : 0;
         let className = "";
